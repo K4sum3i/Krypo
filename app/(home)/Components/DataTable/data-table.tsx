261c476db4e280c31c15ddcb3365ddb2f67beaad
K4sum3i <tezmanuz@gmail.com>
2026-02-28 18:51:33 +0100

    feat: Add @tanstack/react-table for data display and enhance user session handling in the home page

---
+++ app/(home)/Components/DataTable/data-table.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { Element } from "@/lib/generated/prisma/client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuShortcut,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="overflow-hidden rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                className="relative"
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,58 @@
+import { getServerSession } from "next-auth";
 import { HeaderMain } from "./Components/HeaderMain";
 import { AppSidebar } from "@/components/Shared/Sidebar/AppSidebar";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { TableData } from "./Components/DataTable";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession();
+
+  if (!session || !session.user?.email) {
+    return redirect("/");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      email: session?.user.email,
+    },
+    include: {
+      elements: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
+
+  if (!user || !user.elements) redirect("/");
+
   return (
     <div className="flex w-full h-screen bg-background">
       <AppSidebar />
       <main className="flex-1 flex flex-col bg-background min-w-0">
-        <HeaderMain />
+        <HeaderMain userId={user?.id} />
         <div className="flex-1 px-8 overflow-y-auto flex flex-col gap-8">
           <div className="flex flex-col gap-1">
             <h1 className="text-2xl font-semibold">Dashboard</h1>
             <p className="text-muted">
               Here is an overview of your security vault status.
             </p>
+          </div>
+          <div className="grid grid-cols-3 gap-5">
+            <div className="bg-card border border-border rounded-lg p-5 flex flex-col gap-4">
+              b
+            </div>
+            <div className="bg-card border border-border rounded-lg p-5 flex flex-col gap-4">
+              b
+            </div>
+            <div className="bg-card border border-border rounded-lg p-5 flex flex-col gap-4">
+              b
+            </div>
+          </div>
+          <div className="">
+            <h2 className="text-lg font-semibold mb-4">Recently Added</h2>
+            <TableData elements={user.elements} />
           </div>
         </div>
       </main>
